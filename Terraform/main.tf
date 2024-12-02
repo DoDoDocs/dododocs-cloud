d0586bf86315121d1f380369ddf17313eb152456
ShimHyesu <hesushim@gmail.com>
2024-12-02 15:16:13 +0900

    Terraform: cidr 블록 수정

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block           = "172.16.0.0/16"
+  cidr_block           = "192.168.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
@@ -23,52 +23,44 @@ resource "aws_internet_gateway" "igw" {
 # Public Subnet
 resource "aws_subnet" "public_a" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "172.16.0.0/20"
+  cidr_block        = "192.168.0.0/24"
   availability_zone = "ap-northeast-2a"
   map_public_ip_on_launch = true
 
   tags = {
     Name = "dododocs-public-a"
-    "kubernetes.io/role/elb" = "1"
-    "kubernetes.io/cluster/dododocs-cluster" = "shared"
   }
 }
 
 resource "aws_subnet" "public_c" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "172.16.16.0/20"
+  cidr_block        = "192.168.1.0/24"
   availability_zone = "ap-northeast-2c"
   map_public_ip_on_launch = true
 
   tags = {
     Name = "dododocs-public-c"
-    "kubernetes.io/role/elb" = "1"
-    "kubernetes.io/cluster/dododocs-cluster" = "shared"
   }
 }
 
 # Private Subnet
 resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "172.16.128.0/20"
+  cidr_block        = "192.168.100.0/24"
   availability_zone = "ap-northeast-2a"
 
   tags = {
     Name = "dododocs-private-a"
-    "kubernetes.io/role/internal-elb" = "1"
-    "kubernetes.io/cluster/dododocs-cluster" = "shared"
   }
 }
 
 resource "aws_subnet" "private_c" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "172.16.144.0/20"
+  cidr_block        = "192.168.101.0/24"
   availability_zone = "ap-northeast-2c"
 
   tags = {
     Name = "dododocs-private-c"
-    "kubernetes.io/role/internal-elb" = "1"
-    "kubernetes.io/cluster/dododocs-cluster" = "shared"
   }
 }
 
@@ -165,146 +157,4 @@ resource "aws_route_table_association" "private_a" {
 resource "aws_route_table_association" "private_c" {
   subnet_id      = aws_subnet.private_c.id
   route_table_id = aws_route_table.private_a.id
-}
-
-
-# IAM Role for SSM
-resource "aws_iam_role" "ssm_role" {
-  name               = "dododocs-ssm-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      },
-    ]
-  })
-}
-
-resource "aws_iam_policy_attachment" "ssm_policy_attachment" {
-  name       = "ssm-policy-attachment"
-  roles      = [aws_iam_role.ssm_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_instance_profile" "ssm_instance_profile" {
-  name = "dododocs-ssm-instance-profile"
-  role = aws_iam_role.ssm_role.name
-}
-
-resource "aws_security_group" "alb_sg" {
-  name        = "alb-sg"
-  description = "Security group for ALB"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port         = 80
-    to_port           = 80
-    protocol          = "tcp"
-    cidr_blocks       = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port         = 443
-    to_port           = 443
-    protocol          = "tcp"
-    cidr_blocks       = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port         = 0
-    to_port           = 0
-    protocol          = "-1"
-    cidr_blocks       = ["0.0.0.0/0"]
-  }
-
-
-  tags = {
-    Name = "dododocs-alb-sg"
-  }
-}
-
-resource "aws_security_group" "nginx_sg" {
-  name        = "nginx-sg"
-  description = "Security group for Nginx EC2 instance"
-  vpc_id      = aws_vpc.main.id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "dododocs-nginx-sg"
-  }
-}
-
-resource "aws_security_group_rule" "nginx_ingress_from_alb_http" {
-  type                     = "ingress"
-  from_port                = 80
-  to_port                  = 80
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.nginx_sg.id
-  source_security_group_id = aws_security_group.alb_sg.id
-  description              = "Allow HTTP traffic from ALB to Nginx"
-}
-
-resource "aws_security_group_rule" "nginx_ingress_from_alb_https" {
-  type                     = "ingress"
-  from_port                = 443
-  to_port                  = 443
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.nginx_sg.id
-  source_security_group_id = aws_security_group.alb_sg.id
-  description              = "Allow HTTPS traffic from ALB to Nginx"
-}
-
-# EKS Shared Node Security Group 데이터 소스
-data "aws_security_group" "eks_shared_node_sg" {
-  filter {
-    name   = "group-name"
-    values = ["eksctl-dododocs-cluster-cluster-ClusterSharedNodeSecurityGroup-B2TpwuTUXqlH"]
-  }
-}
-
-resource "aws_security_group_rule" "nginx_ingress_from_eks_prometheus" {
-  type                     = "ingress"
-  from_port                = 9090
-  to_port                  = 9090
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.nginx_sg.id
-  source_security_group_id = data.aws_security_group.eks_shared_node_sg.id
-  description              = "Allow Prometheus traffic from EKS nodes to Nginx"
-}
-
-resource "aws_security_group_rule" "nginx_ingress_from_eks_grafana" {
-  type                     = "ingress"
-  from_port                = 3000
-  to_port                  = 3000
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.nginx_sg.id
-  source_security_group_id = data.aws_security_group.eks_shared_node_sg.id
-  description              = "Allow Grafana traffic from EKS nodes to Nginx"
-}
-
-resource "aws_instance" "nginx_instance" {
-  ami                    = "ami-0de20b1c8590e09c5"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.private_a.id
-  key_name               = "dododocs-key"
-  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
-  associate_public_ip_address = false
-  iam_instance_profile   = aws_iam_instance_profile.ssm_instance_profile.name
-
-  depends_on = [aws_security_group.nginx_sg]
-
-  tags = {
-    Name = "dododocs-nginx-instance"
-  }
 }
