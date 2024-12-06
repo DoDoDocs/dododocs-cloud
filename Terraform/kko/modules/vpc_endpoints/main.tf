2e7c92393f19b63023477b756e95da6b96496c17
42kko <kko@student.42seoul.kr>
2024-12-06 11:14:42 +0900

    [feat] terraform::RDS추가

--- Terraform/kko/modules/vpc_endpoints/main.tf
+++ Terraform/kko/modules/vpc_endpoints/main.tf
@@ -14,17 +14,24 @@ locals {
       vpc_endpoint_type = "Interface"
       priavte_dns       = true
     }
+    rds = {
+      service_name      = "com.amazonaws.${var.region}.rds"
+      vpc_endpoint_type = "Interface"
+      private_dns       = true
+    }
   }
 }
 
 resource "aws_vpc_endpoint" "endpoints" {
   for_each = { for service in var.endpoints : service => local.service_config[service] }
 
-  vpc_id             = var.vpc_id
-  service_name       = each.value.service_name
-  subnet_ids         = each.value.vpc_endpoint_type == "Interface" ? var.subnet_ids : null
-  vpc_endpoint_type  = each.value.vpc_endpoint_type
-  security_group_ids = each.value.vpc_endpoint_type == "Interface" ? var.security_group_ids : null
+  vpc_id              = var.vpc_id
+  service_name        = each.value.service_name
+  subnet_ids          = each.value.vpc_endpoint_type == "Interface" ? var.subnet_ids : null
+  vpc_endpoint_type   = each.value.vpc_endpoint_type
+  security_group_ids  = each.value.vpc_endpoint_type == "Interface" ? var.security_group_ids : null
+  private_dns_enabled = lookup(each.value, "private_dns", false)
+
   tags = {
     Name = "${var.name}-${each.key}-endpoint"
   }
